c042bfafdc0c2baded697bf2a1333d81c512e782
Munyaradzi Chikara <99704776+S-Read3r@users.noreply.github.com>
2022-04-28 21:38:45 +0100

    Time: 90 ms (39.37%), Space: 43.5 MB (14.81%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -2,24 +2,29 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-//I thought of using regex, then I realised that the process of making all the combinations would be long. Instead, I opted to using an Obj or Map that has bracket1(br1) as key and br2 as value. 
-    let br = {
-        '(':')','[':']','{':'}'
-    };
+var isValid = function(str) {
+const res = [];
 
-    let heap = [];
-//char for character
-    for(let char of s) {
-        if(br[char]) {
-            heap.push(br[char])
-        }else{
-            if(heap.pop() !== char) {
-                return false
-            }
-        }
+//I will use a for loop to iterate through the given sing
+for(let x in str) {
+
+    //I will use charAt() to pull a character at location(x) from the given string, then use a switch statement which is more efficient than using many else statements. 
+    let pulldChar = str.charAt(x);
+    switch(pulldChar) {
+        case '[': res.push(']');
+            break;
+        case '(': res.push(')');
+            break;
+        case '{': res.push('}');
+            break;
+         default:
+             if (pulldChar !== res.pop()) {
+                 return false
+             }
     }
-    return (!heap.length)
+}
+
+return res.length === 0
 };
 
-console.log(isValid("()[]{}"))
+console.log(isValid("{[()]}"))
