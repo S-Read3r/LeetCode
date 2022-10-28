23f9a4598b38500c784407023bdb0f86fbbc556b
Munyaradzi Chikara <99704776+Syntax-Read3r@users.noreply.github.com>
2022-10-28 21:13:43 +0100

    Time: 114 ms (65.33%), Space: 43.9 MB (15.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,33 +1,15 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-        //I will use a hash map to store what has been seen already, and recall if the number has been seen already.
+function twoSum (arr, target) {
     let map = new Map();
     
-        // I must iterate through the array
-    for(let x = 0; x < nums.length; x++) {
+    for(let x = 0; x < arr.length; x++) {
+        let num1 = arr[x];
+        let num2 = target - num1;
         
-        //I must store the value of location[x] to later use to find the missing number/
-        let cVal = nums[x];
-        
-        // Subtracting cVal from target will reveal the missing number, and then confirm it's existance in the list.
-        let reVal = target - cVal;
-        
-        // I must ask map if it `has` requiredValue(reVal)
-        if(map.has(reVal)) {
-        
-        //Now that the iteration has found the missing number, I (r) their indexs
-            return [x, map.get(reVal)]  //To get the index of the second number, I asked map to .get(reVal)
+        if(map.has(num2)) {
+            return [map.get(num2), x]
+        } else {
+            map.set(num1, x)
         }
-        //If reVal is not found, I used map to set the currentValue and its index(zbi) for recalling/remembering
-        map.set(cVal, x)
         
     }
-};
-
-//Notes: the time complexity is 0 - n, since the fn() is looping through all the numbers. 
-// The space complexity is also o of n because in the worse case, the array could be holding 10,000 numbers.
-//This is giving up memory as we increase these set of numbers.
+}
